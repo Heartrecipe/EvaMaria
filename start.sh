f307341e5950ed0d81d78a49b683c62620fa6da0
PRAKASH <95958624+Heartrecipe@users.noreply.github.com>
2022-02-04 12:10:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Heartrecipe/EvaMaria.git
+  git clone https://github.com/Heartrecipe/EvaMaria.git/EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
